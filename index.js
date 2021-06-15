648407b6405eb390f7fe5b3df0d84071e11beb9c
Garrett Thor <garrettthor@gmail.com>
2021-06-15 09:31:17 -0700

    validation

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+// The catchTryAsync refers to a function which passes error handling through next and down to the error route
+const catchTryAsync = require('./utilities/catchTryAsync');
+const ExpressError = require('./utilities/ExpressError');
 const moment = require('moment');
 const Burrito = require('./models/burrito');
 const PORT = 1984;
@@ -33,7 +36,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
 
-// This middleware makes moment work.  Don't ask me how, thats some stackoverflow copypasta magic babay
+// This middleware makes moment.js work.  Don't ask me how, thats some stackoverflow copypasta magic babay
 app.use((req, res, next) => {
     res.locals.moment = moment;
     next();
@@ -45,43 +48,57 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/burritos', async(req, res) => {
+app.get('/burritos', catchTryAsync(async(req, res) => {
     const burritoList = await Burrito.find({});
     burritoList.sort((a, b) => b.createdAt-a.createdAt);
     res.render('burritos/feed', { burritoList });
-});
+}));
 
 app.get('/burritos/new', (req, res) => {
     res.render('burritos/new');
 });
 
-app.post('/burritos', async(req, res) => {
-    const burrito = new Burrito(req.body.burrito);
-    await burrito.save();
-    res.redirect('burritos');
-});
+app.post('/burritos', catchTryAsync(async(req, res, next) => {
+        if(!req.body.burrito) throw new ExpressError('That\'s not a real burrito...', 400);
+        const burrito = new Burrito(req.body.burrito);
+        await burrito.save();
+        res.redirect('burritos');
+}));
 
-app.get('/burritos/:id', async(req, res) => {
+app.get('/burritos/:id', catchTryAsync(async(req, res) => {
     const burrito = await(Burrito.findById(req.params.id));
     res.render('burritos/display', { burrito })
-});
+}));
 
-app.get('/burritos/:id/edit', async(req, res) => {
+app.get('/burritos/:id/edit', catchTryAsync(async(req, res) => {
     const { id } = req.params;
     const burrito = await Burrito.findById(id);
     res.render(`burritos/edit`, { burrito })
-});
+}));
 
-app.put('/burritos/:id', async(req, res) => {
+app.put('/burritos/:id', catchTryAsync(async(req, res) => {
     const { id } = req.params;
     const burrito = await Burrito.findByIdAndUpdate(id, { ...req.body.burrito });
     res.redirect(`${burrito._id}`)
-});
+}));
 
-app.delete('/burritos/:id', async(req, res) => {
+app.delete('/burritos/:id', catchTryAsync(async(req, res) => {
     const { id } = req.params;
     const burrito = await Burrito.findByIdAndDelete(id);
     res.redirect('/burritos')
+}));
+
+// Error handling
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Whoopsies... something went wrong...'
+    res.status(statusCode).render('error', { err });
 });
 
 // LISTEN

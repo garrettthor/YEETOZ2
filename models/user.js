f492e20140f22b8e6e4ce202ebae345073e8c7d2
Garrett Thor <garrettthor@gmail.com>
2021-06-21 07:32:34 -0700

    post bday notes

--- models/user.js
+++ models/user.js
@@ -16,3 +16,4 @@ module.exports = mongoose.model('User', UserSchema);
 //
 // fixing squaress
 // There was a bit of post-bday depression/anxiety to overcome...
+// I had a little imposter syndrome/encounted some gatekeeping on reddit that shook my confidence a little.

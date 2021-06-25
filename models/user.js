cc4803d4fdbfebae8f6bf6f179dfea94b8609dc6
Garrett Thor <garrettthor@gmail.com>
2021-06-25 07:35:42 -0700

    schedule?

--- models/user.js
+++ models/user.js
@@ -18,3 +18,4 @@ module.exports = mongoose.model('User', UserSchema);
 // There was a bit of post-bday depression/anxiety to overcome...
 // I had a little imposter syndrome/encounted some gatekeeping on reddit that shook my confidence a little.
 // I really need to get this project done so I can move onto React.  MVC has taken me forever wrap my head around.  Never touched the backend before so I suppose thats understandable.
+// I'm going to make a schedule and break the rest of this thing up into chunks so that I have a definative time-line and goals to meet.

fefc65eb08d69b91e26e2344be0495bc805699d2
Garrett Thor <garrettthor@gmail.com>
2021-06-30 07:41:37 -0700

    .

--- models/user.js
+++ models/user.js
@@ -20,7 +20,7 @@ module.exports = mongoose.model('User', UserSchema);
 // I really need to get this project done so I can move onto React.  MVC has taken me forever wrap my head around.  Never touched the backend before so I suppose thats understandable.
 // I'm going to make a schedule and break the rest of this thing up into chunks so that I have a definative time-line and goals to meet.
 // Maybe one week to get the rest of the key features operational.
-// 1. Figure out how to make an account only allow 1 like/disklike
+// 1. Figure out how to make an account only allow 1 like/disklike.
 // One week to ensure responsive design/really tighten up the design.
 // And last week to deploy.
 
